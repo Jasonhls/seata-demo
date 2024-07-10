73a1655912955968b1091f72c132ab76ea5850ad
何立森 <lisen.he@marketingforce.com>
2024-07-10 13:36:09 +0800

    脚本修改

--- ddl.sql
+++ ddl.sql
@@ -1,4 +1,62 @@
+-- seata服务器连接的数据库：
+CREATE TABLE `branch_table` (
+                                `branch_id` bigint NOT NULL,
+                                `xid` varchar(128) NOT NULL,
+                                `transaction_id` bigint DEFAULT NULL,
+                                `resource_group_id` varchar(32) DEFAULT NULL,
+                                `resource_id` varchar(256) DEFAULT NULL,
+                                `branch_type` varchar(8) DEFAULT NULL,
+                                `status` tinyint DEFAULT NULL,
+                                `client_id` varchar(64) DEFAULT NULL,
+                                `application_data` varchar(2000) DEFAULT NULL,
+                                `gmt_create` datetime(6) DEFAULT NULL,
+                                `gmt_modified` datetime(6) DEFAULT NULL,
+                                PRIMARY KEY (`branch_id`),
+                                KEY `idx_xid` (`xid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `distributed_lock` (
+                                    `lock_key` char(20) NOT NULL,
+                                    `lock_value` varchar(20) NOT NULL,
+                                    `expire` bigint DEFAULT NULL,
+                                    PRIMARY KEY (`lock_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `global_table` (
+                                `xid` varchar(128) NOT NULL,
+                                `transaction_id` bigint DEFAULT NULL,
+                                `status` tinyint NOT NULL,
+                                `application_id` varchar(32) DEFAULT NULL,
+                                `transaction_service_group` varchar(32) DEFAULT NULL,
+                                `transaction_name` varchar(128) DEFAULT NULL,
+                                `timeout` int DEFAULT NULL,
+                                `begin_time` bigint DEFAULT NULL,
+                                `application_data` varchar(2000) DEFAULT NULL,
+                                `gmt_create` datetime DEFAULT NULL,
+                                `gmt_modified` datetime DEFAULT NULL,
+                                PRIMARY KEY (`xid`),
+                                KEY `idx_status_gmt_modified` (`status`,`gmt_modified`),
+                                KEY `idx_transaction_id` (`transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `lock_table` (
+                              `row_key` varchar(128) NOT NULL,
+                              `xid` varchar(128) DEFAULT NULL,
+                              `transaction_id` bigint DEFAULT NULL,
+                              `branch_id` bigint NOT NULL,
+                              `resource_id` varchar(256) DEFAULT NULL,
+                              `table_name` varchar(32) DEFAULT NULL,
+                              `pk` varchar(36) DEFAULT NULL,
+                              `status` tinyint NOT NULL DEFAULT '0' COMMENT '0:locked ,1:rollbacking',
+                              `gmt_create` datetime DEFAULT NULL,
+                              `gmt_modified` datetime DEFAULT NULL,
+                              PRIMARY KEY (`row_key`),
+                              KEY `idx_status` (`status`),
+                              KEY `idx_branch_id` (`branch_id`),
+                              KEY `idx_xid` (`xid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- seata_storage数据库：
 DROP TABLE IF EXISTS `storage_tbl`;
 CREATE TABLE `storage_tbl` (
                                `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -8,7 +66,18 @@ CREATE TABLE `storage_tbl` (
                                UNIQUE KEY (`commodity_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `undo_log` (
+                            `branch_id` bigint NOT NULL COMMENT 'branch transaction id',
+                            `xid` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'global transaction id',
+                            `context` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'undo_log context,such as serialization',
+                            `rollback_info` longblob NOT NULL COMMENT 'rollback info',
+                            `log_status` int NOT NULL COMMENT '0:normal status,1:defense status',
+                            `log_created` datetime(6) NOT NULL COMMENT 'create datetime',
+                            `log_modified` datetime(6) NOT NULL COMMENT 'modify datetime',
+                            UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='AT transaction mode undo table';
 
+-- seata_order数据库：
 DROP TABLE IF EXISTS `order_tbl`;
 CREATE TABLE `order_tbl` (
                              `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -19,7 +88,18 @@ CREATE TABLE `order_tbl` (
                              PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `undo_log` (
+                            `branch_id` bigint NOT NULL COMMENT 'branch transaction id',
+                            `xid` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'global transaction id',
+                            `context` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'undo_log context,such as serialization',
+                            `rollback_info` longblob NOT NULL COMMENT 'rollback info',
+                            `log_status` int NOT NULL COMMENT '0:normal status,1:defense status',
+                            `log_created` datetime(6) NOT NULL COMMENT 'create datetime',
+                            `log_modified` datetime(6) NOT NULL COMMENT 'modify datetime',
+                            UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='AT transaction mode undo table';
 
+-- seata_account数据库：
 DROP TABLE IF EXISTS `account_tbl`;
 CREATE TABLE `account_tbl` (
                                `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -27,8 +107,6 @@ CREATE TABLE `account_tbl` (
                                `money` int(11) DEFAULT 0,
                                PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
 
 CREATE TABLE `undo_log` (
                             `branch_id` bigint NOT NULL COMMENT 'branch transaction id',
